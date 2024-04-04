b6ad33ec398d5c2dd230162e844999a5b6684c61
Courtney-Jade <156655989+Courtney-Jade@users.noreply.github.com>
2024-04-05 00:12:09 +0200

    Added on code to javascript

--- Module_8_COUBER437_WFC2401_GroupB_CourtneyBergstedt_JSL08-main/index.js
+++ Module_8_COUBER437_WFC2401_GroupB_CourtneyBergstedt_JSL08-main/index.js
@@ -7,3 +7,24 @@ class BankBranch {
         return BankBranch.bankBranchInstance;
     }
 
+        // Method to get branch information
+        getBranchInfo() {
+            return this.branchInfo;
+        }
+    }
+
+    // Variable to store singleton instance of the bank branch
+BankBranch.bankBranchInstance = null;
+
+// Usage
+const branchA = new BankBranch("Main Street Branch");
+const branchB = new BankBranch("Second Street Branch");
+
+// branch information from branchA
+console.log("Branch A Information:", branchA.getBranchInfo());
+// branch information from branchB
+console.log("Branch B Information:", branchB.getBranchInfo());
+
+// Check if branchA and branchB refer to the same instance
+console.log("Are branchA and branchB the same instance?", branchA === branchB); // Output should be true
+
